d4d8cd8e264617a4642deb8b2b76e78f5d722b46
Andrew Lee <andrew1421lee@gmail.com>
2019-03-11 20:01:36 -0400

    fix how textures are given

--- core/src/com/etirps/zhu/windows95/CardExtensions.kt
+++ core/src/com/etirps/zhu/windows95/CardExtensions.kt
@@ -1,33 +1,33 @@
 package com.etirps.zhu.windows95
 
 import com.badlogic.gdx.graphics.Texture
-import com.badlogic.gdx.graphics.g2d.Sprite
-import com.badlogic.gdx.graphics.g2d.TextureAtlas
 import com.badlogic.gdx.graphics.g2d.TextureRegion
+import com.badlogic.gdx.utils.Disposable
 
-class CardExtensions(cardFaces: Texture, var cardBack: Texture) {
-
+class CardExtensions(var cardFaces: Texture, var cardBack: Texture): Disposable {
     var sprites = TextureRegion(cardFaces).split(409, 585).flatten()
 
-    fun getFront(card: Card): TextureRegion {
-
-        when(card.suit) {
+    fun getFront(suit: Suit, value: Int): TextureRegion {
+        when (suit) {
             Suit.CLOVERS -> {
-                return sprites[card.value]
+                return sprites[value]
             }
             Suit.DIAMONDS -> {
-                return sprites[card.value + 13]
+                return sprites[value + 13]
             }
 
             Suit.HEARTS -> {
-                return sprites[card.value + 26]
+                return sprites[value + 26]
             }
 
             Suit.SPADES -> {
-                return sprites[card.value + 39]
+                return sprites[value + 39]
             }
         }
+    }
 
-
+    override fun dispose() {
+        cardFaces.dispose()
+        cardBack.dispose()
     }
 }
