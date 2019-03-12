be2de5ae5105921dc0cec6876aec797ab38e6ef0
Andrew Lee <andrew1421lee@gmail.com>
2019-03-11 20:01:59 -0400

    drag and drop working...ish

--- core/src/com/etirps/zhu/windows95/GdxGame.kt
+++ core/src/com/etirps/zhu/windows95/GdxGame.kt
@@ -3,11 +3,13 @@ package com.etirps.zhu.windows95
 import com.badlogic.gdx.ApplicationAdapter
 import com.badlogic.gdx.Gdx
 import com.badlogic.gdx.InputProcessor
+import com.badlogic.gdx.graphics.Color
 import com.badlogic.gdx.graphics.GL20
 import com.badlogic.gdx.graphics.OrthographicCamera
 import com.badlogic.gdx.graphics.Texture
 import com.badlogic.gdx.graphics.g2d.SpriteBatch
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
+import com.badlogic.gdx.math.Vector3
 import com.badlogic.gdx.scenes.scene2d.Stage
 import com.badlogic.gdx.utils.viewport.FitViewport
 
@@ -16,12 +18,15 @@ class GdxGame : ApplicationAdapter(), InputProcessor {
     private lateinit var camera: OrthographicCamera
     private lateinit var spriteBatch: SpriteBatch
     private lateinit var shapeRenderer: ShapeRenderer
-
-    private lateinit var cardTextures: CardExtensions
-    private lateinit var img: Texture
+    private lateinit var input: Input
 
     private var screenWidth: Float = 0f
     private var screenHeight: Float = 0f
+
+    private lateinit var cardTextures: CardExtensions
+
+    private lateinit var cards: MutableList<Card>
+    private var focusedCard: Card? = null
 
     override fun create() {
         // Get screen size
@@ -41,10 +46,16 @@ class GdxGame : ApplicationAdapter(), InputProcessor {
 
         // Use this class as the input processor
         Gdx.input.inputProcessor = this
+        input = Input()
 
-        // Load game stuff
+        // Load Textures
         cardTextures = CardExtensions(Texture("cardFaces.png"), Texture("cardBack.png"))
-        img = Texture("badlogic.jpg")
+
+        // Load game objects
+        cards = mutableListOf()
+
+        // Setup game and start
+        setupGame()
     }
 
     override fun render() {
@@ -59,15 +70,19 @@ class GdxGame : ApplicationAdapter(), InputProcessor {
         spriteBatch.projectionMatrix = camera.combined
         shapeRenderer.projectionMatrix = camera.combined
 
-        /////// Test
-        var card = Card(Suit.HEARTS, 0, 0f, 0f)
-        ///////
+        shapeRenderer.begin(ShapeRenderer.ShapeType.Filled)
+        shapeRenderer.color = Color.WHITE
+
+        shapeRenderer.circle(input.destX, input.destY, 5f)
+
+        shapeRenderer.end()
 
         spriteBatch.begin()
 
-        for(i in 0..12) {
-            card.value = i
-            spriteBatch.draw(cardTextures.getFront(card), 150f * i, 100f, card.width * 2, card.height * 2)
+        actOnInput()
+
+        for(card in cards) {
+            spriteBatch.draw(card.textureRegion, card.x, card.y, card.width, card.height)
         }
 
         spriteBatch.end()
@@ -75,11 +90,67 @@ class GdxGame : ApplicationAdapter(), InputProcessor {
 
     override fun dispose() {
         spriteBatch.dispose()
-        img.dispose()
+        cardTextures.dispose()
+    }
+
+    /***** GAME LOGIC FUNCTIONS *****/
+    private fun setupGame() {
+        for(s in 0..0) {
+            // Loop for every suit
+            val suit = Suit.values()[s]
+
+            for(value in 0..12) {
+                // Loop for every value
+                cards.add(Card(suit, value, 150f * value, 100f, 69f * 2, 94f * 2, cardTextures.getFront(suit, value)))
+            }
+        }
+    }
+
+    private fun actOnInput() {
+
+        if(input.fingerUp) {
+            focusedCard = null
+        }
+
+        if(input.draggingCard && focusedCard != null) {
+            focusedCard?.move(input.destX, input.destY)
+        }
+
+        if(!input.draggingCard && focusedCard == null) {
+            for (card in cards) {
+                if(card.bounds.contains(input.destX, input.destY)) {
+                    focusedCard = card
+                    input.draggingCard = true
+                    break
+                }
+            }
+        }
+        
     }
 
     /***** INPUT PROCESSOR FUNCTIONS *****/
+    override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
+        val actualxy = camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(), 0f))
+
+        input.tappedDown(actualxy.x, actualxy.y)
+
+        return true
+    }
+
+    override fun touchDragged(screenX: Int, screenY: Int, pointer: Int): Boolean {
+        val actualxy = camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(), 0f))
+
+        input.dragging(actualxy.x, actualxy.y)
+
+        return true
+    }
+
     override fun touchUp(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
+        val actualxy = camera.unproject(Vector3(screenX.toFloat(), screenY.toFloat(), 0f))
+
+        input.tappedUp(actualxy.x, actualxy.y)
+        focusedCard = null
+
         return true
     }
 
@@ -99,15 +170,9 @@ class GdxGame : ApplicationAdapter(), InputProcessor {
         return true
     }
 
-    override fun touchDragged(screenX: Int, screenY: Int, pointer: Int): Boolean {
-        return true
-    }
-
     override fun keyDown(keycode: Int): Boolean {
         return true
     }
 
-    override fun touchDown(screenX: Int, screenY: Int, pointer: Int, button: Int): Boolean {
-        return true
-    }
+
 }
